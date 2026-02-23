d3b9ef71980c47c9b105d69dacf5e64b2f23b415
adavydov <alexey.davydov@gmail.com>
2026-02-23 15:43:59 +0300

    Fix import-time dataclass error and add import regression check

--- mitra_app/drive.py
+++ mitra_app/drive.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import base64
 import json
 import os
+from dataclasses import dataclass
 from typing import Any
 
 from google.oauth2 import service_account
---
+++ tests/test_imports.py
@@ -0,0 +1,2 @@
+def test_import_main():
+    import mitra_app.main
